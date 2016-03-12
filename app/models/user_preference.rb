71d03b731d48c0f529a45ba86c6c8df0f8419929
Gavin Haynes <gavinh@sfu.ca>
2016-03-12 05:11:35 +0000

    Add validation for user preference skill level

--- app/models/user_preference.rb
+++ app/models/user_preference.rb
@@ -1,4 +1,5 @@
 class UserPreference < ActiveRecord::Base
   belongs_to :user
   belongs_to :sport
+  validates :skill_level, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
--- test/controllers/user_preferences_controller_test.rb
+++ test/controllers/user_preferences_controller_test.rb
@@ -3,11 +3,6 @@ require 'test_helper'
 class UserPreferencesControllerTest < ActionController::TestCase
   setup do
     @user_preference = user_preferences(:one)
-
-#    @basketball = Sport.new(sports(:basketball))
-#    @basketball.save
-#    @soccer = Sport.new(sports(:soccer))
-#    @soccer.save
   end
 
   test "should get index" do
@@ -27,6 +22,17 @@ class UserPreferencesControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to user_preference_path(assigns(:user_preference))
+  end
+
+  test "should reject user_preference with too much skill" do
+    assert_no_difference('UserPreference.count') do
+      @bad_user_preference = user_preferences(:too_skilled)
+      post :create, user_preference: { notes: @bad_user_preference.notes, skill_level: @bad_user_preference.skill_level, sport_id: @bad_user_preference.sport_id, user_id: @bad_user_preference.user_id }
+    end
+  end
+
+  test "should reject user_preference with no skill" do
+    assert_not UserPreference.create(skill_level: user_preferences(:no_skills).skill_level).valid?
   end
 
   test "should show user_preference" do
