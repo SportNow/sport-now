aaf67b6aaae44e32773fe9c57771a7fef04a2ca5
Gavin Haynes <gavinh@sfu.ca>
2016-03-12 00:40:46 +0000

    All tests pass with real test data

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,8 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionController::TestCase
+  # Add the devise helper. 
+  include Devise::TestHelpers
+end
