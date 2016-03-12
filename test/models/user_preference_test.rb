2a9c191d25413b5b8a24c6b4c1f3e1c3ead7ab11
Gavin Haynes <gavinh@sfu.ca>
2016-03-12 23:45:25 +0000

    Add user_preference model test

--- test/models/user_preference_test.rb
+++ test/models/user_preference_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserPreferenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_preference = user_preferences(:one)
+  end
+
+  test "should normally be valid" do
+    assert @user_preference.valid?
+  end
+
+  test "should not be valid if no skills" do
+    assert_not user_preferences(:no_skills).valid?
+  end
 end
